3ba5c183e9490a179ec0850b1d8d8a1bcbb427d0
Mojila <me.mojila@gmail.com>
2020-02-15 21:16:18 +0700

    add echarts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,12 +27,18 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button-example>Adaw bray</button-example>
   </div>
 </template>
 
 <script>
+import ButtonExample from './ButtonExample'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    ButtonExample
+  },
   props: {
     msg: String
   }
